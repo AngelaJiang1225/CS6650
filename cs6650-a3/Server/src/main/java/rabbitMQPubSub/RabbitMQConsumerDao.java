72fbe2f1b6ba676a57bb4ade01904f1e59dc4d4b
Aiqi Jiang <aiqi.jiang@inceptio.ai>
2020-12-01 14:49:20 +0800

    final modification

--- cs6650-a3/Server/src/main/java/rabbitMQPubSub/RabbitMQConsumerDao.java
+++ cs6650-a3/Server/src/main/java/rabbitMQPubSub/RabbitMQConsumerDao.java
@@ -1,17 +1,9 @@
-package RabbitMQConsumer;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-import javax.sql.DataSource;
+package main.java.rabbitMQPubSub;
 
 import io.swagger.client.model.LiftRide;
 
 public class RabbitMQConsumerDao {
+
     JDBConnection jdbcObj;
     DataSource dataSource;
 
